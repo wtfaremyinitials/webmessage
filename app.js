c8326e6db8678396a9e9d2f033dc054a5e74d74b
Will Franzen <wtfranzen@gmail.com>
2014-11-20 23:10:34 -0600

    Implement POST /send

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 var express = require('express');
 var app = express();
+
+var iMessage = require('osa-imessage');
+
+var port = process.env.PORT || 5677;
+
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+    res.end('Hello World');
+});
+
+app.post('/send', function(req, res){
+    var to = req.body.to;
+    var message = req.body.message;
+
+    iMessage.send(message, to, function(err, data) {
+        if(err) {
+            res.statusCode = 500;
+            res.json({
+                success: false
+            });
+        } else {
+            res.statusCode = 200;
+            res.json({
+                success: true,
+                to: data
+            });
+        }
+    });
+});
+
+app.listen(port);
+
+console.log('Server started on port: ' + port);

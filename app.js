08ca2b192ab87c4ff63804cd89ec5921f2f9d7d5
Will Franzen <wtfranzen@gmail.com>
2014-11-30 17:07:52 -0600

    Do work on app.js - Add CORS support - Serve ./static - Add TODOs

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ var port = process.env.PORT || 5677;
 app.use(require('body-parser').json());
 app.use(require('body-parser').urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authentication");
+    next();
+});
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/auth', function(req, res) {
+    // TODO: Test if the Authentication header is correct
+});
+
 app.post('/send', function(req, res){
+    // TODO: Test if the Authentication header is correct
+
     var to = req.body.to;
     var message = req.body.message;
 
